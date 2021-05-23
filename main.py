9ea40464ab2b5fe715cb6c5f083f92a8d41c7bda
Bhavya0020 <bhavya.learncode@gmail.com>
2021-05-23 21:07:39 +0530

    Create Invisible Cloak

---
+++ main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+
+time.sleep(3)
+
+bg = 0
+
+for i in range(30):
+
+    ret,bg = cap.read()
+
+while(cap.isOpened()):
+    ret,img = cap.read()
+
+    if(not ret):
+        break
+    
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # hsv = hue saturation value
+    lower_red = np.array([0,120,50])
+    upper_red = np.array([10,255,255])
+    mask1 = cv2.inRange(hsv,lower_red,upper_red)
+
+    lower_red = np.array([170,120,70])
+    upper_red = np.array([180,255,255])
+    mask2 = cv2.inRange(hsv,lower_red,upper_red)
+    
+    mask1 = mask1 + mask2
+
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN,
+                np.ones((3,3),np.uint8),iterations = 2)
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE,
+                np.ones((3,3),np.uint8),iterations = 2)
+    
+    mask2 = cv2.bitwise_not(mask1)
+    
+    res1 = cv2.bitwise_and(bg, bg, mask = mask1) 
+    res2 = cv2.bitwise_and(img, img, mask = mask2) 
+    out = cv2.addWeighted(res1, 1, res2, 1, 0) 
+  
+    cv2.imshow("INVISIBLE MAN", out) 
+    k = cv2.waitKey(10) 
+    if k == 27: 
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
